0c431210cef5076f9d403071480c2876b2df503b
uznik <krasavecnikki@gmail.com>
2022-10-22 17:17:20 +0400

    Added homework for the 3 seminar and corrections to the 2 seminar

---
+++ Seminar2_correct/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет. БЕЗ ИСПОЛЬЗОВАНИЯ СТРОК
+
+int FindThirdDigit(int number){
+    int numberMin = 100;
+    int numberMax = 1000;
+    int divider = 1;
+    while (true) {
+        if (numberMin <= number & number < numberMax){
+        int result = number / divider % 10;
+        return result;
+        } else {   
+            numberMin *= 10;
+            numberMax *= 10;
+            divider *= 10;
+        }
+    }
+}
+
+Console.Write("Input number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number > 100){
+    int result = FindThirdDigit(number);
+    Console.WriteLine(result);
+} else{
+     Console.WriteLine("Третьей цифры нет");
+}
+
+// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// void Request(){
+//     Console.Write("Input number: ");
+//     int number = Convert.ToInt32(Console.ReadLine());
+    
+//     if ( 0 < number & number < 8){
+//         CheckWeekends(number);
+//     } else {
+//         Console.WriteLine("Вы совершили ошибку ввода, пожалуйста, введите число от 1 до 7: ");
+//         Request();
+//     }
+// }
+
+// void CheckWeekends(int number){
+//     if ( number > 5){
+//          Console.WriteLine("Да");
+//     } else{
+//         Console.WriteLine("Нет");
+//     }
+// }   
+
+// Request();
---
+++ Seminar3/Program.cs
@@ -0,0 +1,75 @@
+﻿//Задача 19. Напишите программу, которая принимает на вход пятизначное число
+//и проверяет, является ли оно палиндромом.
+
+// void Palindrome(int number){
+//     int num1 = number / 1000;
+//     int num2 = number % 100 / 10 + number % 10 * 10; // Выявляем последнюю и предпоследнюю цифру числа и меняем их местами
+    
+//     if (num1 == num2){
+//         Console.WriteLine("Да");
+//     } else Console.WriteLine("Нет");
+// }
+
+// void Request(){
+//     Console.Write("Input a five-digit number: ");
+//     int number = Convert.ToInt32(Console.ReadLine());
+
+//     if (10000 <= number & number < 100000){
+//         Palindrome(number);
+//     } else{
+//         Console.WriteLine("При вводе данных была допущена ошибка, пожалуйста повторите ввод: ");
+//         Request();
+//     }
+// }
+
+// Request();
+
+
+//Задача 21. Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+
+// double Distance(int xa, int ya, int za, int xb, int yb, int zb){
+//     double abc = Math.Sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya) + (zb - za) * (zb - za));
+//     return abc;
+// } 
+
+// Console.Write("Введите значение xa: ");
+// int xa = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение ya: ");
+// int ya = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение za: ");
+// int za = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите значение xb: ");
+// int xb = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение yb: ");
+// int yb = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение zb: ");
+// int zb = Convert.ToInt32(Console.ReadLine());
+
+
+// double distance = Distance(xa,ya,za,xb,yb,zb);
+// Console.WriteLine($"Расстояние между точками ({xa}, {ya}), ({xb}, {yb}) и ({za},{zb}) равно {distance}");
+
+
+//Задача 23. Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// void CubesOfNumbers(int number){
+//     for ( int i = 1; i <= number; i++){
+//         Console.WriteLine($"{i*i*i} ");
+//     }
+// }
+
+// void NegativeCubesOfNumbers(int number){
+//     for ( int i = -1; i >= number; i--){
+//         Console.WriteLine($"{i*i*i} ");
+//     }
+// }
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number == 0) Console.WriteLine("0");
+// else if (number < 0) NegativeCubesOfNumbers( number );
+// else CubesOfNumbers( number );
